d0d54f32ecf291bace7304bf2a8e82820cebbb69
Siddhant Chourasia <siddhantchaurasia1999@gmail.com>
2020-12-07 21:15:53 +0530

    Added pin unpin functionality.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,11 +4,13 @@ import axios from "../axios-notes";
 import Button from "@material-ui/core/Button";
 import DeleteIcon from "@material-ui/icons/Delete";
 import RestoreIcon from '@material-ui/icons/Restore';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
 
 
 const useStyles = makeStyles({
@@ -20,7 +22,7 @@ const useStyles = makeStyles({
         display: "inline-block"
     },
     title: {
-      textAlign: "center"
+        textAlign: "center"
     },
     description: {
         position: "relative",
@@ -31,48 +33,74 @@ const useStyles = makeStyles({
 const Todo = (props) => {
     const classes = useStyles();
 
-    const toBinHandler = (e) => {
+    const toBinHandler = () => {
         let tempTodo = {...props.todos};
         let data = {
             bin: true,
             title: props.title,
             description: props.description,
-            pinned: props.todos[props.index].pinned,
+            pinned: false,
         }
-        axios.put('/mynotes/'+ props.index+'.json', data)
+        axios.put('/mynotes/' + props.index + '.json', data)
             .then((response) => {
                 tempTodo[props.index].bin = true;
-                console.log(tempTodo)
                 props.setTodos(tempTodo);
             })
             .catch(error => console.log(error));
     }
-    const toMyNotesHandler = (e) => {
+
+    const toMyNotesHandler = () => {
         let tempTodo = {...props.todos};
         let data = {
             bin: false,
             title: props.title,
             description: props.description,
-            pinned: props.todos[props.index].pinned,
+            pinned: false,
         }
-        axios.put('/mynotes/'+ props.index+'.json', data)
+        axios.put('/mynotes/' + props.index + '.json', data)
             .then((response) => {
                 tempTodo[props.index].bin = false;
+                props.setTodos(tempTodo);
+            })
+            .catch(error => console.log(error));
+    }
+
+    const pinHandler = () => {
+        let tempTodo = {...props.todos};
+
+        let data = {
+            bin: false,
+            title: props.title,
+            description: props.description,
+            pinned: !props.todos[props.index].pinned,
+        }
+        axios.put('/mynotes/' + props.index + '.json', data)
+            .then((response) => {
+                tempTodo[props.index].pinned = !props.todos[props.index].pinned;
                 console.log(tempTodo)
                 props.setTodos(tempTodo);
             })
             .catch(error => console.log(error));
     }
-    let buttons = props.bin ? <span className="binCrossMark"><Button onClick={toMyNotesHandler}><RestoreIcon /></Button></span> :
-        <span className="binCrossMark"><Button onClick={toBinHandler}><DeleteIcon  /></Button></span>
-
+    console.log(props.todos[props.index].pinned)
+    let buttons = props.bin ?
+        <span className="binCrossMark"><Button onClick={toMyNotesHandler}><RestoreIcon/></Button></span> :
+        <div>
+            <span className="binCrossMark"><Button onClick={toBinHandler}><DeleteIcon/></Button></span>
+            {props.todos[props.index].pinned ?
+                <span className="binCrossMark"><Button
+                    onClick={pinHandler}><LocationOnIcon/></Button></span> :
+                <span className="binCrossMark"><Button onClick={pinHandler}><LocationOnOutlinedIcon/></Button></span>
+            }
+        </div>
     return (
         <Card className={classes.root}>
             <CardContent>
                 <Typography className={classes.title} variant="h5" component="h2" gutterBottom>
                     {props.title}
                 </Typography>
-                <hr/><br/>
+                <hr/>
+                <br/>
                 <Typography className={classes.description} variant="body2" component="p">
                     {props.description}
                 </Typography>
